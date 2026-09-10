2ac20b4b570e1c09a52078805d6777bc5d5e6258
David Alexander 000224179 <dalexa7@wgu.edu>
2026-09-10 16:57:41 -0500

    Replace favicon with app icon

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -100,7 +100,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg?v=2", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
